ed85927b0a4ffa8e3f92d4f03b5d375c6fd3873d
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2024-06-14 09:41:36 +0530

    start patterns

---
+++ Day 4 June 13 Nested For Loops/Estar Pattern/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

008147ec92fd556a81849659a07e1c7acf46643a
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2024-06-19 09:06:19 +0530

    Find max in an array

---
+++ Day 5 June 14 Arrays/Matrix/Find max/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 324, 45, 90, 9808}; 
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) if (arr[i] > max) max = arr[i];
+
+        System.out.println(max);
+    }
+}
